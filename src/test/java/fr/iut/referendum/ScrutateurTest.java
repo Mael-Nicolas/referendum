2ccaa209d498c21e6e17e4585bb5fc1d45cc26c8
Killian RAMUS <killian.ramus@etu.umontpellier.fr>
2024-10-09 15:34:27 +0200

    feat[print-1] : fonction encrypt qui ne marche pas sur le test que j'ai fais mais le code semble intéréssant à garder pour la base

--- src/test/java/fr/iut/referendum/ScrutateurTest.java
+++ src/test/java/fr/iut/referendum/ScrutateurTest.java
@@ -1,7 +1,18 @@
 package fr.iut.referendum;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigInteger;
 
 class ScrutateurTest {
-
+    @org.junit.jupiter.api.Test
+    void testEncrypt() {
+        BigInteger m = new BigInteger("123456789");
+        BigInteger g = new BigInteger("2");
+        BigInteger p = new BigInteger("101");
+        BigInteger q = new BigInteger("5");
+        BigInteger publicKey = new BigInteger("10");
+        BigInteger[] result = Scrutateur.encrypt(m, g, p, q, publicKey);
+        assertEquals(new BigInteger("32"), result[0]);
+        assertEquals(new BigInteger("50"), result[1]);
+    }
 }
