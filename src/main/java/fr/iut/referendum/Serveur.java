54ec948603e59edc2edff2dd06f16dc0bdbd05bb
Cyprien Bons <cyprien.bons@etu.umontpellier.fr>
2024-12-05 16:00:19 +0100

    feat[Sprint3]: socket sécurisée serveur

--- src/main/java/fr/iut/referendum/Serveur.java
+++ src/main/java/fr/iut/referendum/Serveur.java
@@ -1,6 +1,12 @@
 package fr.iut.referendum;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 import java.math.BigInteger;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -62,28 +68,48 @@ public class Serveur {
         return result;
     }
 
-    public static void main(String[] args) {
-        // Certificat SSL
-        System.setProperty("javax.net.ssl.keyStore", "keystore.jks");
-        System.setProperty("javax.net.ssl.keyStorePassword", "Admin!123");
+    public static void main(String[] args) throws NoSuchAlgorithmException, KeyStoreException, FileNotFoundException, UnrecoverableKeyException, KeyManagementException {
+        try {
+            // Certificat SSL
+            System.setProperty("javax.net.ssl.keyStore", "keystore.jks");
+            System.setProperty("javax.net.ssl.keyStorePassword", "Admin!123");
 
-        try (ServerSocket serverSocket = new ServerSocket(3390)) {
-            System.out.println("Server open on port " + serverSocket.getLocalPort());
+            // Initialisation de SSLContext
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            KeyStore keyStore = KeyStore.getInstance("JKS");
 
-            InetAddress adrLocale = InetAddress.getLocalHost();
-            System.out.println("Adresse locale = "+ adrLocale.getHostAddress());
-            System.out.println("Nom de la machine locale = "+ adrLocale.getHostName());
-
-            Referendum r1 = new Referendum("Killian président ?", new Date(2025-1900, Calendar.JANUARY, 1, 0, 0));
-            Referendum r2 = new Referendum("Vincent revienne a Montpellier ?", new Date(2028-1900, Calendar.FEBRUARY, 29, 20, 0));
-            Referendum r3 = new Referendum("Ouverture BL3 ?", new Date(2024-1900, Calendar.NOVEMBER, 12, 17, 38));
-            Serveur serveur = new Serveur(new ArrayList<>(List.of(r1, r2, r3)));
-            while (true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("Connexion établie avec " + socket.getInetAddress());
-                new ServerThread(socket, serveur).start();
+            try (FileInputStream keyStoreFile = new FileInputStream("keystore.jks")) {
+                keyStore.load(keyStoreFile, "Admin!123".toCharArray());
+            } catch (CertificateException e) {
+                throw new RuntimeException(e);
             }
 
+            keyManagerFactory.init(keyStore, "Admin!123".toCharArray());
+            sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+
+            // Utilisation d'un SSLServerSocket
+            SSLServerSocketFactory serverSocketFactory = sslContext.getServerSocketFactory();
+            SSLServerSocket serverSocket = (SSLServerSocket) serverSocketFactory.createServerSocket(3390);
+
+            // Affichage Info serveur
+            System.out.println("Serveur sécurisé ouvert sur le port " + serverSocket.getLocalPort());
+            InetAddress adrLocale = InetAddress.getLocalHost();
+            System.out.println("Adresse locale = " + adrLocale.getHostAddress());
+            System.out.println("Nom de la machine locale = " + adrLocale.getHostName());
+
+            // Création de référendums de test
+            Referendum r1 = new Referendum("Killian président ?", new Date(2025 - 1900, Calendar.JANUARY, 1, 0, 0));
+            Referendum r2 = new Referendum("Vincent revient à Montpellier ?", new Date(2028 - 1900, Calendar.FEBRUARY, 29, 20, 0));
+            Referendum r3 = new Referendum("Ouverture BL3 ?", new Date(2024 - 1900, Calendar.NOVEMBER, 12, 17, 38));
+            Serveur serveur = new Serveur(new ArrayList<>(List.of(r1, r2, r3)));
+
+            // Boucle de gestion des connexions clients
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Connexion sécurisée établie avec " + socket.getInetAddress());
+                new ServerThread(socket, serveur).start();
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
